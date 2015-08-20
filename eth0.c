ca4f1895904ea70d856e7ff5ecf124e86ff6dd4c
Madez <faleey92@outlook.com>
2015-08-21 01:48:41 +0800

    Update eth0.c

--- eth0.c
+++ eth0.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
-#include <string.h> 
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <arpa/inet.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-
-int main(){
-
-FILE *fp;
-
-char returnData[64];
-const char s= "*";
-char *token;
-
-token = strtok(returnData, s);
-
-fp = popen("/sbin/ifconfig eth0", "r");
-
-while (fgets(returnData, 64, fp) token != NULL)
+int main()
 {
-    printf("%s", token);
-    
-    token = strtok(NULL, s);
-}
-
-pclose(fp);
+	FILE* fp;
+	char returnData[64];
+        
+	fp = popen("/sbin/ifconfig eth0", "r");
+	char x[100][100];
+	int i = 0;
+	
+	while (fgets(returnData, 64, fp) != NULL)
+	{
+     char* token = strtok(returnData, ":");
+		while(token != NULL)
+		{			
+			strcpy(x[i], token);
+			i++;
+    		token = strtok(NULL," ");
+		}	      
+	}
+	printf("Local IP Address : %s \n", x[6]);
+	pclose(fp);
 }
